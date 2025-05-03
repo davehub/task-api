81eefc962abae258162e730af471f10337e10142
davehub <dave.developpeur.dave@gmail.com>
2025-05-03 10:20:37 +0000

    newcommit

---
+++ api/tasks/post.ts
@@ -0,0 +1,9 @@
+import { VercelRequest } from "@vercel/node";
+import { VercelResponse } from "@vercel/node";
+
+export default async function handler(
+  req: VercelRequest,
+  res: VercelResponse
+) {
+  res.status(200).json({ message: "Hello World !" });
+}
---
+++ server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import { VercelRequest } from "@vercel/node";
+import { VercelResponse } from "@vercel/node";
+const app = express();
+const PORT = 3000;
+
+// Configuration CORS
+const corsOptions = {
+    origin: process.env.FRONTEND_URL ||"*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  };
+  
+  // middeware
+  app.use(cors(corsOptions));
+  app.use(express.json());
+
+  const tasksHandler = require("./api/tasks").default;
+
+  app.all("/api/tasks", async (req: express.Request, res: express.Response) => {
+    await tasksHandler(req, res);
+  });
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
